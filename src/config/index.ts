e0463009b0944c72c75779af9d625814f669007b
evgeni <evgeni@ravin-ai.com>
2020-10-26 19:29:27 +0200

    API_ENDPOINT set to dev remote server

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://192.168.31.127:8081/api',
+    API_ENDPOINT: 'https://npuzzle-backend.herokuapp.com/api',
     DEFAULT_N: 15,
     APP_NAME: `N-Puzzle`,
     WELCOME_MESSAGE: `Drag and drop the tiles!`,
